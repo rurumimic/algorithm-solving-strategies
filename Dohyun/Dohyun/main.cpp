d3fb3503f49400d7ee8921e62a0a7692412cbe53
Dodo <unhyop@gmail.com>
2018-03-11 18:55:03 +0900

    Init

---
+++ Dohyun/Dohyun/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Dohyun
+//
+//  Created by Dohyun Kim on 2018. 3. 11..
+//  Copyright © 2018년 Rurumimic. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
