ad9b257d6d35740a90e1bdb53b462f2dbfcd2bb0
Dodo <unhyop@gmail.com>
2018-03-14 17:52:39 +0900

    editing

--- Dohyun/Chapter01/main.cpp
+++ Dohyun/Chapter01/main.cpp
@@ -1,87 +1,86 @@
-//
-//  main.cpp
-//  Dohyun
-//
-//  Created by Dohyun Kim on 2018. 3. 11..
-//  Copyright © 2018년 Rurumimic. All rights reserved.
-//
-
 #include <iostream>
 #include <vector>
 
 using namespace std;
 
-double inputData();
-double compute(int days, int teams, vector<int> rentfee);
+int days;
+int teams;
+int cost;
+vector<int> rentfee;
 
-double inputData() {
-    int days = 0;
-    int teams = 0;
-    int cost = 0;
-    
+void makeData();
+double compute();
+
+void makeData() {
+    days = 0;
+    teams = 0;
+    cost = 0;
+
     cin >> days >> teams;
-    
-    vector<int> rentfee(days);
-    
-    for(int i=0; i<days; i++) {
+
+    rentfee.resize(days);
+
+    for(int i = 0; i < days; i++) {
         cin >> cost;
         rentfee[i] = cost;
     }
-    
-    return compute(days, teams, rentfee);
+
+    return;
 }
 
-double compute(int days, int teams, vector<int> rentfee) {
-    
-    int i, j, k, t = 0;
+double compute() {
+    int i = 0;
+    int begin = 0;
     double count = 0.0;
     double sum = 0.0;
-    double m = 987654321.0;
-    double temp = 0.0;
-    double result = 987654321.0;
-    
-    for(i = 0; i < days-teams+1; i++) {
+    double baseMean = 987654321.0;
+    double tempMean = 0.0;
+    double resultMean = 987654321.0;
+
+    for(i = 0; i <= days-teams; i++) {
         sum = 0;
-        for(j = 0; j < teams; j++) {
-            t = j + i;
-            sum = sum + rentfee[t];
-        }
-        
-        m = sum / teams;
-        if (m < result) {
-            result = m;
-        }
         count = teams;
-        
-        for(k = t+1; k < days; k++) {
+
+        for(begin = i; begin < teams; begin++) {
+          sum = sum + rentfee[begin];
+        }
+
+        baseMean = sum / teams;
+
+        if (baseMean < resultMean) {
+            resultMean = baseMean;
+        }
+
+        for(; begin < days; begin++) {
             count++;
-            sum = sum + rentfee[k];
-            temp = sum / count;
-            if (temp < result) {
-                result = temp;
+            sum = sum + rentfee[begin];
+            tempMean = sum / count;
+            if (tempMean < resultMean) {
+                resultMean = tempMean;
             }
         }
     }
-    
-    return result;
+
+    return resultMean;
 }
 
 int main(int argc, const char * argv[]) {
     int testcase;
-    
+
     cin >> testcase;
     vector<double> result(testcase);
-    
+
     for(int i = 0; i < testcase; i++) {
-        result[i] = inputData();
+        makeData();
+        result[i] = compute();
     }
-    
+
     cout << fixed;
     cout.precision(12);
-    
+
     for(int j = 0; j < testcase; j++) {
         cout << result[j] << endl;
     }
-    
+
     return 0;
 }
