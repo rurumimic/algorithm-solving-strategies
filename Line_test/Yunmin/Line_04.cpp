4b0ab55a25c46da85a59ebe864096a33a8778237
grblake-PC\grblake <yunmin1992@sogang.ac.kr>
2018-04-02 16:36:05 +0900

    tc추가

--- Line_test/Yunmin/Line_04.cpp
+++ Line_test/Yunmin/Line_04.cpp
@@ -22,47 +22,51 @@ uint64_t inverse(uint64_t K) {
 }
 
 int main(void) {
-	uint64_t N, K, M;
+	int tc;
+	uint32_t N, K, M;
 	uint64_t down_inverse1, down_inverse2, ans, _up, _down1, _down2;
 
-	scanf("%llu %llu %llu", &N, &K, &M);
+	scanf("%d", &tc);
 
-	_up = _down1 = _down2 = 1;
+	for (int t = 0; t < tc; t++) {
+		scanf("%llu %llu %llu", &N, &K, &M);
 
-	for (uint32_t i = 1; i <= N; i++) {
-		_up *= i;
-		_up %= mod;
+		_up = _down1 = _down2 = 1;
+
+		for (uint32_t i = 1; i <= N; i++) {
+			_up *= i;
+			_up %= mod;
+		}
+
+		for (uint32_t i = 1; i <= M; i++) {
+			_down1 *= i;
+			_down1 %= mod;
+		}
+
+		for (uint32_t i = 1; i <= (N - M); i++) {
+			_down1 *= i;
+			_down1 %= mod;
+		}
+
+		for (uint32_t i = 1; i <= K - M; i++) {
+			_down2 *= i;
+			_down2 %= mod;
+		}
+
+		for (uint32_t i = 1; i <= (N - K + M); i++) {
+			_down2 *= i;
+			_down2 %= mod;
+		}
+
+		down_inverse1 = inverse(_down1);
+		down_inverse2 = inverse(_down2);
+
+		down_inverse1 = (down_inverse1 * _up) % mod;
+		down_inverse2 = (down_inverse2 * _up) % mod;
+
+		ans = (down_inverse1 * down_inverse2) % mod;
+
+		printf("%llu\n", ans);
 	}
-
-	for (uint32_t i = 1; i <= M; i++) {
-		_down1 *= i;
-		_down1 %= mod;
-	}
-
-	for (uint32_t i = 1; i <= (N - M); i++) {
-		_down1 *= i;
-		_down1 %= mod;
-	}
-
-	for (uint32_t i = 1; i <= K - M; i++) {
-		_down2 *= i;
-		_down2 %= mod;
-	}
-
-	for (uint32_t i = 1; i <= (N - K + M); i++) {
-		_down2 *= i;
-		_down2 %= mod;
-	}
-
-	down_inverse1 = inverse(_down1);
-	down_inverse2 = inverse(_down2);
-
-	down_inverse1 = (down_inverse1 * _up) % mod;
-	down_inverse2 = (down_inverse2 * _up) % mod;
-
-	ans = (down_inverse1 * down_inverse2) % mod;
-
-	printf("%llu\n", ans);
-
 	return 0;
 }
